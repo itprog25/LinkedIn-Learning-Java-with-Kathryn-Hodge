842afcbd5e40720a52cedbfed0fe9e577365b331
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-08-24 12:58:25 +0530

    primitive data types

---
+++ LearningJava/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int studentAge = 22;
+        double studentGPA = 7.63;
+        char studentFirstInitial = 'S';
+        char studentLastInitial = 'N';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
