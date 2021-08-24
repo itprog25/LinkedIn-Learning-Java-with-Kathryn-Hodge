7afdef8e36acd9ca9e54d800a58fae5ef905d37f
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-08-24 13:04:40 +0530

    reference data types

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+
         /* primitive data types
         1: int
         2: double
@@ -8,16 +8,23 @@ public class Main {
         4: char
         */
 
+        /* reference data types
+        1: String
+
+        */
+
         int studentAge = 22;
         double studentGPA = 7.63;
-        char studentFirstInitial = 'S';
-        char studentLastInitial = 'N';
+        char studentFirstInitial = 'J';
+        char studentLastInitial = 'S';
         boolean hasPerfectAttendance = true;
+        String studentName = "John Smith";
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentName);
     }
 }
