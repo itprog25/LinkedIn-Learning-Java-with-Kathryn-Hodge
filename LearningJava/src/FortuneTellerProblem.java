71f6a84b0a585dad65938cdcd0dc74651fb7d57d
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-08-24 14:17:56 +0530

    FortuneTellerProblem

---
+++ LearningJava/src/FortuneTellerProblem.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FortuneTellerProblem {
+    public static void main(String[] args) {
+        System.out.println("Pick a number between 1 and 10");
+        Scanner input = new Scanner(System.in);
+        int pickedNumber = input.nextInt();
+
+        
+    }
+}
