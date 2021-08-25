943ac31221f228ba4802d8fa6f24fc8250927ca8
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-08-25 08:48:43 +0530

    Testing out challenge

---
+++ LearningJava Kathryn Hodge/src/TestingOut.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TestingOut {
+    public static void main(String[] args) {
+        String question = "Which one is a language?";
+        String choiceOne = "Windows 10";
+        String choiceTwo = "Java 15";
+        String choiceThree = "Mac OS X";
+
+        String answer = "Java 15";
+
+        System.out.println(question);
+        System.out.println("The choices are :" + " " + choiceOne + " "+ choiceTwo + " "+ choiceThree);
+
+        Scanner input = new Scanner(System.in);
+        String choice = input.nextLine();
+        input.close();
+
+        if(choice.equals(answer.toLowerCase())) {
+            System.out.println("Congrats, the answer is correct");
+        } else {
+            System.out.println("The answer is incorrect. Correct answer is :"+ answer);
+        }
+    }
+}
