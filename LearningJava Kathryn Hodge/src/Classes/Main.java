23b383171f9e6a356045d7d042a733459347dc5d
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-08-25 11:23:44 +0530

    Instances in java

---
+++ LearningJava Kathryn Hodge/src/Classes/Main.java
@@ -0,0 +1,10 @@
+package Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle TriangleA = new Triangle(15,8,15,8,17);
+        Triangle TriangleB = new Triangle(3,2.598,3,3,3);
+
+        
+    }
+}
