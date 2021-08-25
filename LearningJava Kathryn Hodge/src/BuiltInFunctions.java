9e5c44ba6bb7eeb30fb66682eec3039c6c90c4ea
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-08-25 08:58:10 +0530

    Built in Functions

---
+++ LearningJava Kathryn Hodge/src/BuiltInFunctions.java
@@ -0,0 +1,9 @@
+public class BuiltInFunctions {
+    public static void main(String[] args) {
+        double result = Math.pow(2,5);
+        System.out.println("Result of 2 ^ 5 is "+result);
+
+        int max = Math.max(2, 3);
+        System.out.println("Max of 2 and 3 is "+max);
+    }
+}
