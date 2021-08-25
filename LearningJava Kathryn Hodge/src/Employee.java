00368da3bf1692f77adb56565617b9f7622183b1
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-08-25 09:17:07 +0530

    My solution to Employee Problem

---
+++ LearningJava Kathryn Hodge/src/Employee.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Employee {
+    public static int calculateSalary() {
+        System.out.println("Enter employee name");
+        Scanner input = new Scanner(System.in);
+        String employeeName = input.nextLine();
+        System.out.println("Enter the number of hours employee works per week.");
+        int hourWeek = input.nextInt();
+        System.out.println("Enter the amount and employee makes per an hour");
+        int amountPerHour = input.nextInt();
+        System.out.println("Enter number of days employee takes vacation");
+        int vacationDays = input.nextInt();
+        input.close();
+        System.out.println("Total yearly gross Salary of Employee "+employeeName);
+        int totalVacationDaysSalary = vacationDays * 8;
+        int totalSalary = hourWeek * amountPerHour * 52 - totalVacationDaysSalary;
+        return totalSalary;
+    }
+    public static void main(String[] args) {
+        int grossSalary = calculateSalary();
+        System.out.println("is "+ grossSalary);
+    }
+}
