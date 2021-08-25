f53fd22889323240a0169017113922cbf91c35f4
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-08-25 09:32:06 +0530

    Solution by Kathryn Hodge

---
+++ LearningJava Kathryn Hodge/src/EmployeeProblemSolution.java
@@ -0,0 +1,22 @@
+public class EmployeeProblemSolution {
+    
+    public static double salaryCalculator(double hoursPerWeek, double amountPerHour, int vacationDays) {
+
+        if (hoursPerWeek < 0) {
+            return -1;
+        }
+
+        if (amountPerHour < 0) {
+            return -1;
+        }
+
+        double weeklyPayCheck = hoursPerWeek * amountPerHour;
+        double unpaidTime = vacationDays * amountPerHour * 8;
+        return (weeklyPayCheck * 52) - unpaidTime;
+        
+    }
+    public static void main(String[] args) {
+        double salary = salaryCalculator(40,15,8);
+        System.out.println(salary);
+    }
+}
