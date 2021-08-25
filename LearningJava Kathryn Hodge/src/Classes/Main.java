c083b22143ad378eeb2b67965f097c6a204c10f6
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-08-25 11:27:10 +0530

    Update Main.java

--- LearningJava Kathryn Hodge/src/Classes/Main.java
+++ LearningJava Kathryn Hodge/src/Classes/Main.java
@@ -2,9 +2,10 @@ package Classes;
 
 public class Main {
     public static void main(String[] args) {
-        Triangle TriangleA = new Triangle(15,8,15,8,17);
-        Triangle TriangleB = new Triangle(3,2.598,3,3,3);
+        Triangle triangleA = new Triangle(15,8,15,8,17);
+        Triangle triangleB = new Triangle(3,2.598,3,3,3);
 
-        
+        double triangleAArea = triangleA.findArea();
+        System.out.println("Area of Triangle A is " + triangleAArea);
     }
 }
