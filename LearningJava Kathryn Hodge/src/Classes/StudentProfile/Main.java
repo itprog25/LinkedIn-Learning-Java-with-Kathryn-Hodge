18e300e07ca5d4c717a5740c29a5de2f1b147f27
Shibin Thomas <josephthomas948@gmail.com>
2021-08-26 20:45:42 +0530

    Main Class Student Details

--- LearningJava Kathryn Hodge/src/Classes/StudentProfile/Main.java
+++ LearningJava Kathryn Hodge/src/Classes/StudentProfile/Main.java
@@ -5,9 +5,12 @@ public class Main {
         Student studentA = new Student("Tom","Sam",2020,7.63,"BCA");
         Student studentB = new Student("Bob","Bob",2019,8.93,"BCom Finance");
 
-        int updatedGraduationYear = studentA.incrementExpectedGraduationYear();
+        int studAUpdatedGraduationYear = studentA.incrementExpectedGraduationYear();
         System.out.println("Student Details are :");
         System.out.println("Student Name: "+studentA.studFirstName);
-        System.out.println("Student Expected Graduation Year: " +updatedGraduationYear);
+        System.out.println("Student Last Name: " +studentA.studLastName);
+        System.out.println("Student GPA: "+studentA.studGPA);
+        System.out.println("Student Major: "+studentA.studMajor);
+        System.out.println("Student Expected Graduation Year: " +studAUpdatedGraduationYear);
     }
 }
